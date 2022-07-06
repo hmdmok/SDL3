803307cd33dd6585c061ac1ddc992e1958bd78d5
Moknine <hmd.moknine@gmail.com>
2022-07-06 15:38:52 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dossiers = require("./DataBase/dossiers");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Server is running ...");
+});
+
+app.get("/api/dossiers", (req, res) => {
+  res.json(dossiers);
+});
+
+app.get("/api/dossiers/:id", (req, res) => {
+  const dossier = dossiers.find((d) => d.id === req.params.id);
+  res.json(dossier);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server is listening at PORT: ${PORT}`));
